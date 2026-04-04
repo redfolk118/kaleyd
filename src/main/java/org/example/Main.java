1d363a49894480ba4c333fa98007cb6888b57180
redfolk118 <118lions@gmail.com>
2026-04-05 00:37:17 +0700

    changed logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-
-        // ВАЖНО: отключаем очистку webhook
-        System.setProperty("org.telegram.telegrambots.disableWebhook", "true");
-
-        botsApi.registerBot(new Bot());
-
-        System.out.println("Бот запущен!n");
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new CampBot());
+            System.out.println("Bot started!");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
